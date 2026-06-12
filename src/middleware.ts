3a63a7bac1e3574cb967f2aac48c094ff62e72a0
tadrisintehran-beep <tadrisintehran_github@gmail.com>
2026-06-12 14:48:34 +0330

    fix build errors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,1 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
-export async function middleware(request: NextRequest) {
-  return NextResponse.next()
-}
+export { }
